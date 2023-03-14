f682ec888c1abc9f9187bd97d2abff5be781d731
´José Roberto Jr <joserobertodsj@gmail.com>
2023-03-14 16:29:01 -0300

    feat: create product model

---
+++ src/main/java/com/api/delivery/models/Product.java
@@ -0,0 +1,32 @@
+package com.api.delivery.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+    private String imageUri;
+
+
+
+}
