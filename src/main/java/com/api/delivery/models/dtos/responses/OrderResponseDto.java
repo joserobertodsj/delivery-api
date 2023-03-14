34a6b680317d16ab6464b55b287224eca6b74cef
´José Roberto Jr <joserobertodsj@gmail.com>
2023-03-14 17:35:24 -0300

    feat: create response dto for order

---
+++ src/main/java/com/api/delivery/models/dtos/responses/OrderResponseDto.java
@@ -0,0 +1,25 @@
+package com.api.delivery.models.dtos.responses;
+
+import com.api.delivery.enums.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderResponseDto {
+
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private Instant moment;
+    private OrderStatus status;
+    private List<ProductResponseDto> products = new ArrayList<>();
+}
