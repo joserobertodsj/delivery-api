110954ccf78af03163feb785e87c86e363a9142f
´José Roberto Jr <joserobertodsj@gmail.com>
2023-03-14 17:18:41 -0300

    feat: add annotations in product model

--- src/main/java/com/api/delivery/models/Product.java
+++ src/main/java/com/api/delivery/models/Product.java
@@ -1,9 +1,6 @@
 package com.api.delivery.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +13,7 @@ import java.io.Serializable;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "tb_product")
 public class Product implements Serializable {
     private static final long serialVersionUID = 1L;
 
